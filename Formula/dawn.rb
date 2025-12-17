a6a401952b174afd8a87ca1320c6db25ec8fa44c
Andrew Sampson <1297077+andrewmd5@users.noreply.github.com>
2025-12-17 11:41:27 +0900

    Update dawn.rb

--- Formula/dawn.rb
+++ Formula/dawn.rb
@@ -1,17 +1,28 @@
 class Dawn < Formula
   desc "Distraction-free terminal writing environment with live markdown rendering"
   homepage "https://github.com/andrewmd5/dawn"
-  version "0.0.9"
+  version "VERSION"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/andrewmd5/dawn/releases/download/v0.0.9/dawn-macos-arm64.tar.gz"
-      sha256 "bef140a1a96994029153dca8c00b1750b9a5a764fb9db2dc68d7bb40e8a29e8a"
+      url "https://github.com/andrewmd5/dawn/releases/download/vVERSION/dawn-macos-arm64.tar.gz"
+      sha256 "SHA256_MACOS_ARM64"
     end
     on_intel do
-      url "https://github.com/andrewmd5/dawn/releases/download/v0.0.9/dawn-macos-x64.tar.gz"
-      sha256 "bfbec9980a6e6a42812713883ae605cb1d2808ffc2042eb445aa9e0a7282ed21"
+      url "https://github.com/andrewmd5/dawn/releases/download/vVERSION/dawn-macos-x64.tar.gz"
+      sha256 "SHA256_MACOS_X64"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/andrewmd5/dawn/releases/download/vVERSION/dawn-linux-arm64.tar.gz"
+      sha256 "SHA256_LINUX_ARM64"
+    end
+    on_intel do
+      url "https://github.com/andrewmd5/dawn/releases/download/vVERSION/dawn-linux-x64.tar.gz"
+      sha256 "SHA256_LINUX_X64"
     end
   end
 
